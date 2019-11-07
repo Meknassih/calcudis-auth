f8e098ec6aff2fca5c30049398c906d70055de6c
Hamza El Meknassi <elmeknassi.h@gmail.com>
2019-11-07 17:17:45 +0100

    Login works

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const sqlite = require('sqlite');
+const express = require('express');
+const router = express.Router();
+const openDb = sqlite.open('./data.db', { Promise });
 
-/* GET users listing. */
-router.post('/login', function (req, res, next) {
+router.post('/login', async function (req, res, next) {
   console.log('>>BODY:', req.body);
-  console.log('>>USER:', req.body.user);
-  res.send('respond with a resource');
+
+  try {
+    const db = await openDb;
+    console.log('Sucessfully connected to DB');
+    const row = await db.get('SELECT * FROM user WHERE username = ? AND password = ?',
+      [req.body.username, req.body.password]);
+    res.setHeader('Content-type', 'application/json');
+    if (row) {
+      res.status(200);
+      res.send(JSON.stringify(row));
+    } else {
+      res.status(400);
+      res.send(JSON.stringify({ message: "Bad username/password combination." }));
+    }
+  } catch (err) {
+    res.status(500);
+    res.send('Internal server error.');
+  }
 });
 
 module.exports = router;
