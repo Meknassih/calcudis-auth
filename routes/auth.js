b4b6287f3bcd1206c72b0cf68c251ac9288da541
Hamza El Meknassi <elmeknassi.h@gmail.com>
2019-11-07 14:39:42 +0100

    Added SQLite schema

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.post('/login', function (req, res, next) {
+  console.log('>>BODY:', req.body);
+  console.log('>>USER:', req.body.user);
   res.send('respond with a resource');
 });
 
---
+++ schema.sql
@@ -0,0 +1,1 @@
+CREATE TABLE user (username varchar(32), password varchar(32), email varchar(32));
